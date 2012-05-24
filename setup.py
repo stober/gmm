1aefa57c8f45b44be9a854451fb40c9e959e4f0f
Jeremy Stober <stober@gmail.com>
2012-05-23 21:45:57 -0500

    Added proper setup.py file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+"""
+@author: stober
+"""
+
+
+from distutils.core import setup
+
+setup(name='gmm',
+      version='0.1',
+      description='Gaussian Mixture Models',
+      author='Jeremy Stober',
+      author_email='stober@gmail.com',
+      package_dir={'gmm':'src'},
+      packages=['gmm'],
+      )
+
+
+
---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+"""
+Author: Jeremy M. Stober
+Program: __INIT__.PY
+Date: Wednesday, May 23 2012
+"""
+
+
+from normal import Normal
+from gmm import GMM, shownormal
+
